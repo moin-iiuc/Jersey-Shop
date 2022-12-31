149994ac161f44d050d58cf96901f9492deffc0b
moin-iiuc <mohammadmhbhuiyan@gmail.com>
2022-12-31 13:44:03 +0600

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from './Bannar/Bannar'
+import Services from './Services/Services'
+import Footer from './Footer'
+export default function Home() {
+  return (
+    <>
+    <div>
+       
+        
+        <Footer></Footer>
+    </div>
+    </>
+  )
+}
---
+++ src/Components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from '../Footer'
+const Reviews = () => {
+  return (
+    <>
+    
+    <Footer></Footer>
+    </>
+  )
+}
+export default Reviews
